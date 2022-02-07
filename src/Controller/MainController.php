2131b62be8e6a86101a28bca820df403bc1e6128
Aladiel <jonathan.mougin0@gmail.com>
2022-02-07 17:30:38 +0100

    petites corrections affichages et liens

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Service\CallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class MainController extends AbstractController
 {
+
     /**
      * @Route ("/", name="main_home")
      */
-    public function home()
+    public function home(CallApiService $callApiService): Response
     {
-        return $this->render('main\home.html.twig');
+        return $this->render('main\home.html.twig', [
+            'data' => $callApiService -> getVillesData()
+    ]);
     }
+
+
 
 }
--- src/Service/CallApiService.php
+++ src/Service/CallApiService.php
@@ -2,9 +2,24 @@
 
 namespace App\Service;
 
+
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class CallApiService {
 
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this -> client = $client;
+    }
+
     public function getVillesData(): array {
-        return [];
+        $response = $this -> client -> request(
+            'GET',
+            'https://geo.api.gouv.fr/communes?nom=&fields=departement&boost=population&limit=5'
+        );
+        return $response -> toArray();
     }
 }
