4967ad28e908ac360acf4fed2a01df207a6f98b5
Adrien Bodin <AdrienBodin@Hotmail.com>
2022-02-07 16:38:12 +0100

    Ajout du rôle Admin, de son dashboard etc.
    Signed-off-by: Adrien Bodin <AdrienBodin@Hotmail.com>

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -93,11 +93,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+      return $this->roles;
 
-        return array_unique($roles);
+
     }
 
     public function setRoles(array $roles): self
